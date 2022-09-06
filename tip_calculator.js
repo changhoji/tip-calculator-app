215f9e1cd883f54df65caf61e584f78ae2f144da
2021035487 <onkkno1@gmail.com>
2022-09-06 15:13:20 +0900

    Update: not radio version

---
+++ tip_calculator.js
@@ -0,0 +1,11 @@
+var tip_text = document.querySelector("#tip-result>.result-price");
+var total_text = document.querySelector("#total-result>.reuslt-price");
+
+
+function test(self) {
+   tip_text.style.color = "powderblue";
+}
+
+
+
+//js 실행시점과 관련된 내용: https://velog.io/@ssket/Script%EC%9D%98-%EC%8B%A4%ED%96%89-%EC%8B%9C%EC%A0%90-%EC%A1%B0%EC%A0%88%ED%95%98%EA%B8%B0
